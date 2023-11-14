b2c882f38ac660e4bc7990fc689ec7173339f651
romeo <rdumlao07@gmail.com>
2023-11-14 12:06:17 -0500

    added console.table dependency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 //Console Table
-const cTable = require('console.table');
+require('console.table');
 
 // Import and require mysql2
 const mysql = require('mysql2');
