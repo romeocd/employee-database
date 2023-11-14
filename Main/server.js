8ad0020c6ac9b518114c3f5ec3d4629e859d50a0
romeo <rdumlao07@gmail.com>
2023-11-13 21:08:19 -0500

    added function to begin inquirer prompts

--- Main/server.js
+++ Main/server.js
@@ -18,4 +18,60 @@ const db = mysql.createConnection(
 db.connect((err) => {
     if (err) throw err;
     console.log(`Connected to the 'employees_db database.`);
-})
+
+    //Begin the prompts after connecting to the database
+    beginPrompt();
+});
+
+//Function to begin inquirer prompts
+function beginPrompt () {
+    inquirer
+    .prompt([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update Employee Role',
+        ]
+     }
+    ])
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View All Departments':
+                //function to view all departments
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                //function to view all roles
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                //function to view all employees
+                viewAllEmployees();
+                break;
+            case 'Add a Department':
+                //function to add a department
+                addDepartment();
+                break;
+            case 'Add a Role':
+                //function to add a role
+                addRole();
+                break;
+            case 'Add an Employee':
+                //function to add an employee
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                //function to update employee role
+                updateEmployeeRole();
+                break;
+        }
+    })
+}
