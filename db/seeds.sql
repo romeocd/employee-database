a36a9c2eb81b5337b3d1c628e33c84290c41acbe
romeo <rdumlao07@gmail.com>
2023-11-14 11:18:28 -0500

    updated values for role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,15 @@ USE employees_db;
 INSERT INTO department (id, name) VALUES (1, 'sales');
 INSERT INTO department (id, name) VALUES (2, 'engineering');
 INSERT INTO department (id, name) VALUES (3, 'finance');
-INSERT INTO department (id, name) VALUES (4, 'sales');
+INSERT INTO department (id, name) VALUES (4, 'legal');
 
+--role
+INSERT INTO role (title, salary, department_id) VALUES
+('Sales Lead', 100000, 1);
+('Salesperson', 80000, 1);
+('Lead Engineer', 150000, 2);
+('Software Engineer', 120000, 2);
+('Account Manger', 160000, 3);
+('Accountant', 125000, 3);
+('Legal Team Lead', 250000, 4);
+('Lawyer', 190000, 4);
