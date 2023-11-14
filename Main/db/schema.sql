6b84504337c49291f62ff1fdd326ec98729dc86e
romeo <rdumlao07@gmail.com>
2023-11-13 21:11:39 -0500

    update role and employee tables

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -5,26 +5,23 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(10, 2),
     department_id INT,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id),
-  REFERENCES role(id),
-  FOREIGN KEY (manager_id),
-  REFERENCES employee(id)
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
